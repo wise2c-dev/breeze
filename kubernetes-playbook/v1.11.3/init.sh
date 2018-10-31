b9b31a00fb033d33c17ad718bc34b04a102a2b86
iexception007 <6098550@qq.com>
2018-10-31 10:24:17 +0800

    add new pki

--- kubernetes-playbook/v1.11.3/init.sh
+++ kubernetes-playbook/v1.11.3/init.sh
@@ -93,3 +93,12 @@ docker save ${dashboard_repo}/kubernetes-dashboard-amd64:${dashboard_version} \
 rm ${path}/file/dashboard.tar.bz2 -f
 bzip2 -z --best ${path}/file/dashboard.tar
 echo "=== save dashboard image success ==="
+
+echo "=== download cfssl tools ==="
+export CFSSL_URL=https://pkg.cfssl.org/R1.2
+curl -L -o cfssl ${CFSSL_URL}/cfssl_linux-amd64
+curl -L -o cfssljson ${CFSSL_URL}/cfssljson_linux-amd64
+curl -L -o cfssl-certinfo ${CFSSL_URL}/cfssl-certinfo_linux-amd64
+chmod +x cfssl cfssljson cfssl-certinfo
+tar zcvf ${path}/file/cfssl-tools.tar.gz cfssl cfssl-certinfo cfssljson
+echo "=== cfssl tools is download successfully ==="

03d472a7290da32bb15865a53a2e3b4e62026cbf
iexception007 <6098550@qq.com>
2018-11-01 16:09:44 +0800

    add time 30

--- kubernetes-playbook/v1.11.3/file/ipvs.sh
+++ kubernetes-playbook/v1.11.3/file/ipvs.sh
@@ -2,7 +2,7 @@
 
 kubectl -n kube-system get cm kube-proxy -o yaml|sed 's/mode: ".*"/mode: "ipvs"/g'|kubectl -n kube-system replace -f -
 
-while true; do
+for i in {1..30}; do
 DESIRED=`kubectl -n kube-system get ds kube-proxy | awk 'NR==2{print $2}'`
 AVAILABLE=`kubectl -n kube-system get ds kube-proxy | awk 'NR==2{print $6}'`
     if [ "${DESIRED}"x == "${AVAILABLE}"x ]; then
@@ -11,7 +11,7 @@ AVAILABLE=`kubectl -n kube-system get ds kube-proxy | awk 'NR==2{print $6}'`
     fi
 
     echo "DESIRED=$DESIRED, AVAILABLE=$AVAILABLE"
-    sleep 2
+    sleep 1
 done
 
 kubectl -n kube-system delete pod $(kubectl -n kube-system get pod | grep 'kube-proxy' | awk '{print $1}')

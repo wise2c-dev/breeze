9f1aef436e78d60fa1c3bc17656f4019cad24074
aimuz <mr.imuz@gmail.com>
2019-06-10 17:07:50 +0800

    fix istio docker.io

--- istio-playbook/version-images/init.sh
+++ istio-playbook/version-images/init.sh
@@ -15,7 +15,7 @@ curl -L -o ${path}/file/istio-$IstioVersion-origin.tar.gz https://github.com/ist
 
 cd ${path}/file/
 tar zxf istio-$IstioVersion-origin.tar.gz
-cat istio-$IstioVersion/install/kubernetes/istio-demo.yaml | grep "image:" |grep -v '\[\[' |awk -F':' '{print $2":"$3}' | grep istio | awk -F "[\"\"]" '{print $2}' | sort | uniq > images-list.txt
+cat istio-$IstioVersion/install/kubernetes/istio-demo.yaml | grep "image:" |grep -v '\[\[' |awk -F':' '{print $2":"$3}' | grep istio | awk -F "[\"\"]" '{print $2}' | sort | uniq | sed 's/docker.io\///g' > images-list.txt
 cat istio-$IstioVersion/samples/bookinfo/platform/kube/bookinfo.yaml | grep "image:" | awk '{ print $2}' >> images-list.txt
 
 for file in $(cat images-list.txt); do docker pull $file; done

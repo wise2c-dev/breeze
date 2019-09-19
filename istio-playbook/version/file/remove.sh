8de69473635e28f66a4b9f83ef2f8b4723cdd4ac
aimuz <mr.imuz@gmail.com>
2019-09-19 16:06:56 +0800

    update k8s init, and add istio image

--- istio-playbook/version/file/remove.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-helm delete --purge istio
-helm delete --purge istio-init
-kubectl delete -f /var/tmp/wise2c/istio/istio-*/install/kubernetes/helm/istio-init/files
-kubectl delete ns istio-system

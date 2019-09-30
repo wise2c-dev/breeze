58a252c29b39090d56e417320625857030ed7e78
aimuz <mr.imuz@gmail.com>
2019-09-30 16:22:03 +0800

    update build file

--- init.sh
+++ init.sh
@@ -4,24 +4,25 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=$(cat /tmp/config.yaml    |  yq -r '.branchs[] | select(.branch == "release-1.15")|.kube_version')
-docker_version=docker-$(cat /tmp/config.yaml |  yq -r '.branchs[] | select(.branch == "release-1.15")|.docker_version')
-haproxy_version=$(cat /tmp/config.yaml       |  yq -r '.branchs[] | select(.branch == "release-1.15")|.haproxy_version')
-keepalived_version=$(cat /tmp/config.yaml    |  yq -r '.branchs[] | select(.branch == "release-1.15")|.keepalived_version')
+version=$1
+
+kubernetes_version=$(cat /tmp/config.yaml    |  yq -r ".branchs[] | select(.branch == \"${version}\")|.kube_version")
+docker_version=docker-$(cat /tmp/config.yaml |  yq -r ".branchs[] | select(.branch == \"${version}\")|.docker_version")
+haproxy_version=$(cat /tmp/config.yaml       |  yq -r ".branchs[] | select(.branch == \"${version}"\)|.haproxy_version")
+keepalived_version=$(cat /tmp/config.yaml    |  yq -r ".branchs[] | select(.branch == \"${version}\")|.keepalived_version")
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
-istio_version=$(cat /tmp/config.yaml         |  yq -r '.branchs[] | select(.branch == "release-1.15")|.istio_version')
-flannel_version=$(cat /tmp/config.yaml       |  yq -r '.branchs[] | select(.branch == "release-1.15")|.flannel_version')
-dashboard_version=$(cat /tmp/config.yaml     |  yq -r '.branchs[] | select(.branch == "release-1.15")|.dashboard_version')
+istio_version=$(cat /tmp/config.yaml         |  yq -r ".branchs[] | select(.branch == \"${version}\")|.istio_version")
+flannel_version=$(cat /tmp/config.yaml       |  yq -r ".branchs[] | select(.branch == \"${version}\")|.flannel_version")
+dashboard_version=$(cat /tmp/config.yaml     |  yq -r ".branchs[] | select(.branch == \"${version}\")|.dashboard_version")
+
+docker run --rm --name=kubeadm-version wisecloud/kubeadm-version:${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
+etcd_version=`cat ${path}/k8s-images-list.txt |grep etcd |awk -F ':' '{print $2}'`
 
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/${kubernetes_version}
 mv ${path}/docker-playbook/version ${path}/docker-playbook/${docker_version}-CE
 mv ${path}/istio-playbook/version-images ${path}/istio-playbook/v${istio_version}
-
-docker run --rm --name=kubeadm-version wisecloud/kubeadm-version:${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
-
-etcd_version=`cat ${path}/k8s-images-list.txt |grep etcd |awk -F ':' '{print $2}'`
-mv etcd-playbook/version-by-kubeadm etcd-playbook/${etcd_version}
+mv ${path}/etcd-playbook/version-by-kubeadm etcd-playbook/${etcd_version}
 
 echo "ETCD Version: ${etcd_version}" > ${path}/components-version.txt
 echo "Kubernetes Version: ${kubernetes_version}" >> ${path}/components-version.txt
